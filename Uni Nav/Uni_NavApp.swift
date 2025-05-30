ab3aedf1900d0839d8d6da5add3f43e812d3131e
IshanKM <ishanmadushanka.sg@gmail.com>
2025-05-29 21:42:52 -0700

    Initial commit

---
+++ Uni Nav/Uni_NavApp.swift
@@ -0,0 +1,17 @@
+//
+//  Uni_NavApp.swift
+//  Uni Nav
+//
+//  Created by ISHAN  on 2025-05-29.
+//
+
+import SwiftUI
+
+@main
+struct Uni_NavApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Uni NavTests/Uni_NavTests.swift
@@ -0,0 +1,17 @@
+//
+//  Uni_NavTests.swift
+//  Uni NavTests
+//
+//  Created by ISHAN  on 2025-05-29.
+//
+
+import Testing
+@testable import Uni_Nav
+
+struct Uni_NavTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
